261df23e3d48f7078271238e0babd34fa7efba5d
Matheus <mfscontato2196@gmail.com>
2022-01-19 08:10:29 -0300

    first-commit

---
+++ js/js.js
@@ -0,0 +1,58 @@
+// function areaQuadrado(lado) {
+//     var area = lado * lado;
+//     return area;
+// }
+
+// console.log(areaQuadrado(2));
+
+// function imc (peso, altura) {
+//     var imc = peso / (altura * altura);
+//     return imc;
+// }
+
+// function pickAColor(color) {
+//     if(color == 'Blue') {
+//         console.log('You like the sky');
+//     }
+    
+//     else if(color == 'Red') {
+//         console.log('You are a romantic person');
+//     }
+    
+//     else if(color == 'Green') {
+//         console.log('You are a holpefully person!');
+//     }
+// }
+
+// function howManyCountries(visited) {
+//     let countriesWorld = 195;
+//     let totalCountries = countriesWorld - visited;
+
+//     console.log("There's still " + totalCountries + " countrie for you to visited");
+// }
+
+
+// for(var i = 0; i < customers.length; i++) {
+    //     console.log(customers[i]);
+    //     if(customers[i] == "Donalt Trump") {
+        //         console.log("Chegou no FDP");
+        //     }
+        // }
+        
+// var customers = ["Jonny Cash", "Donalt Trump", "Boby Dylan", "Janis Joplin"];
+// var i = 0
+// while(i < customers.length) {
+//     console.log[customers[i]];
+//     i++;
+// }
+
+// let number1 = 10;
+// let number2 = 20;
+
+// let highestNumber = (n1, n2) => n1 > n2 ? n1: n2;
+
+// console.log(highestNumber(number1, number2));
+
+// function ePaisagem (largura, altura) {
+//     if()
+// }
