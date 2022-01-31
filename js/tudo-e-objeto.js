c2258cdb830e8fb572be40a4801793f1b99fd752
matheus-fernando <matheus.fernando.coder@gmail.com>
2022-01-31 10:09:07 -0300

    More things about objects

---
+++ js/tudo-e-objeto.js
@@ -0,0 +1,19 @@
+var btn = document.querySelector('.btn');
+btn.addEventListener('click', () => alert('Hello, world'));
+
+//Exercícios
+
+// Primeiro
+// var myName = "Matheus";
+// console.log(myName.toLowerCase());
+// console.log(myName.toUpperCase());
+// console.log(myName.replace('us', 'nós'));
+
+// Segundo
+btn.remove('.btn');
+btn.getElementsByClassName('.btn');
+btn.addEventListener('click', () => {});
+btn.before('.btn');
+btn.after('.btn');
+//Terceiro
+// execCommand();
