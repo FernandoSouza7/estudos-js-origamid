90b50709eb0edfde8adf12d5f86c4c25bbe8b02b
matheus-fernando <matheus.fernando.coder@gmail.com>
2022-02-04 10:13:32 -0300

    Learning about DOM

---
+++ dom/dom.js
@@ -0,0 +1,30 @@
+// let firstH1 = document.querySelector('h1');
+
+// firstH1.addEventListener('click', () => {
+//     alert('Successfuly');
+// });
+
+// firstH1.addEventListener('click', newMessage);
+
+// function newMessage(){
+//     const newMessageH1 = firstH1.innerText='successfully changed';
+//     console.log(newMessageH1);
+// }
+
+const url = window.location.href;
+console.log(url);
+
+const active = document.querySelector('.ativo');
+console.log(active);
+
+let browserLang = navigator.language;
+console.log("The language is: " + browserLang);
+
+let windowHeight = window.innerHeight;
+console.log(windowHeight);
+
+
+
+
+
+
---
+++ js/ternario.js
@@ -0,0 +1,8 @@
+// var booleano = 'falso';
+// var valorTernario = booleano == true || booleano == 'true' ? 'verdadeiro': 'falso';
+
+// console.log(valorTernario);
+
+value = valor;
+
+console.log(value);
